037d8d105541153645d729959b09ed0eb50c2de5
Nikita <niita.nstu@gmail.com>
2020-05-17 02:24:01 +0700

    add derivatives counter

--- GradientDescent/src/Newton.h
+++ GradientDescent/src/Newton.h
@@ -38,26 +38,16 @@ result_info lambda_newton(newton_info& info, std::vector<vec2>& points)
 
 	points.push_back(x0);
 
-	CSV csv(100, 100);
-
-	csv(0, 0, '(');
-	csv(0, 0, x0.x);
-	csv(0, 0, ',');
-	csv(0, 0, x0.y);
-	csv(0, 0, ')');
-
-	csv(0, 1, f(x0));
-
-	int line = 2;
 	while (diff_x >= delta && diff_f >= eps && result.iter_count != maxiter)
 	{
-
 		mat2 H = mat2(h, x0);
+		result.calc_count += 4;
 		// Calculate second partial derivative matrix
 		mat2 H_inv = H.inverse();
 
 		// Calculate gradient
 		vec2 grad(g, x0);
+		result.calc_count += 2;
 
 		// Calculate direction
 		vec2 mult = H_inv * grad;
@@ -71,66 +61,16 @@ result_info lambda_newton(newton_info& info, std::vector<vec2>& points)
 		vec2 x1 = x0 + lambda * dx;
 		points.push_back(x1);
 
-		csv(line, 0, '(');
-		csv(line, 0, x1.x);
-		csv(line, 0, ',');
-		csv(line, 0, x1.y);
-		csv(line, 0, ')');
-
-		csv(line, 1, f(x1));
-
-		csv(line - 2, 2, '(');
-		csv(line - 2, 2, dx.x);
-		csv(line - 2, 2, ',');
-		csv(line - 2, 2, dx.y);
-		csv(line - 2, 2, ')');
-
-		csv(line - 2, 3, lambda);
-
-		csv(line, 4, abs(x1.x - x0.x));
-		csv(line, 4, ", ");
-		csv(line, 4, abs(x1.y - x0.y));
-		csv(line, 4, ", ");
-		csv(line, 4, abs(f(x1) - f(x0)));
-
-		csv(line - 2, 5, '(');
-		csv(line - 2, 5, grad.x);
-		csv(line - 2, 5, ',');
-		csv(line - 2, 5, grad.y);
-		csv(line - 2, 5, ')');
-
-		csv(line - 2, 6, H.a11);
-		csv(line - 2, 7, H.a12);
-
-		csv(line - 1, 6, H.a21);
-		csv(line - 1, 7, H.a22);
-
-		line += 2;
 		// Calculate ||x[k + 1] - x[k]||
 		diff_x = (x1 - x0).norm();
 
 		// Calculate |f[k + 1] - f[k]|
 		diff_f = abs(f(x1) - f(x0));
+		result.calc_count += 2;
 		x0 = x1;
 		result.iter_count++;
 	}
 
-	vec2 grad = vec2(g, x0);
-	mat2 H = mat2(h, x0);
-
-	csv(line - 2, 5, '(');
-	csv(line - 2, 5, grad.x);
-	csv(line - 2, 5, ',');
-	csv(line - 2, 5, grad.y);
-	csv(line - 2, 5, ')');
-
-	csv(line - 2, 6, H.a11);
-	csv(line - 2, 7, H.a12);
-
-	csv(line - 1, 6, H.a21);
-	csv(line - 1, 7, H.a22);
-	
-	//csv.Write("secondRes.csv");
 	return result;
 }
 
--- GradientDescent/src/main.cpp
+++ GradientDescent/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <cmath>
 
@@ -6,97 +7,124 @@
 #include "Newton.h"
 #include "Broyden.h"
 
-func f = [](vec2 x) { return 100 * (x.y - x.x) * (x.y - x.x) + (1 - x.x) * (1 - x.x); };
+func f = [](vec2 x) { return 100.0 * (x.y - x.x) * (x.y - x.x) + (1.0 - x.x) * (1.0 - x.x); };
 
 grad g =
 {
-	[](vec2 x) { return -200 * (x.y - x.x) - 2 * (1 - x.x); },
-	[](vec2 x) { return 200 * (x.y - x.x); }
+	[](vec2 x) { return -200.0 * (x.y - x.x) - 2.0 * (1.0 - x.x); },
+	[](vec2 x) { return 200.0 * (x.y - x.x); }
 };
 
 hesse h =
 {
-	[](vec2 x) { return 202; },
-	[](vec2 x) { return -200; },
-	[](vec2 x) { return -200; },
-	[](vec2 x) { return 200; }
+	[](vec2 x) { return 202.0; },
+	[](vec2 x) { return -200.0; },
+	[](vec2 x) { return -200.0; },
+	[](vec2 x) { return 200.0; }
 };
 
 func f1 = [](vec2 x) { return 100.0 * (x.y - x.x * x.x) * (x.y - x.x * x.x) + (1.0 - x.x) * (1.0 - x.x); };
 
 grad g1 =
 {
-[](vec2 x) { return -400.0 * x.x * (x.y - x.x * x.x) + 2.0 * (x.x - 1.0); },
-[](vec2 x) { return 200.0 * (x.y - x.x * x.x); }
+	[](vec2 x) { return -400.0 * x.x * (x.y - x.x * x.x) + 2.0 * (x.x - 1.0); },
+	[](vec2 x) { return 200.0 * (x.y - x.x * x.x); }
 };
 
 hesse h1 =
 {
-[](vec2 x) { return -400.0 * (x.y - 3.0 * x.x * x.x) + 2.0; },
-[](vec2 x) { return -400.0 * x.x; },
-[](vec2 x) { return -400.0 * x.x; },
-[](vec2 x) { return 200.0; }
+	[](vec2 x) { return -400.0 * (x.y - 3.0 * x.x * x.x) + 2.0; },
+	[](vec2 x) { return -400.0 * x.x; },
+	[](vec2 x) { return -400.0 * x.x; },
+	[](vec2 x) { return 200.0; }
 };
 
 
 double exp1(double x, double y)
 {
-	return exp(-(x - 1) * (x - 1) / 4 - (y - 1) * (y - 1));
+	return exp(-(x - 1.0) * (x - 1.0) / 4.0 - (y - 1.0) * (y - 1.0));
 }
 
 double exp2(double x, double y)
 {
-	return exp(-(x - 2) * (x - 2) / 9 - (y - 3) * (y - 3) / 4);
+	return exp(-(x - 2.0) * (x - 2.0) / 9.0 - (y - 3.0) * (y - 3.0) / 4.0);
 }
 
-func f2 = [](vec2 x) { return -(2 * exp1(x.x, x.y) + 3 * exp2(x.x, x.y)); };
+func f2 = [](vec2 x) { return -(2.0 * exp1(x.x, x.y) + 3.0 * exp2(x.x, x.y)); };
 
 grad g2 =
 {
-	[](vec2 x) { return -((1 - x.x) * exp1(x.x, x.y) + 2.0 / 3 * (2 - x.x) * exp2(x.x, x.y)); },
-	[](vec2 x) { return -(4 * (1 - x.y) * exp1(x.x, x.y) + 3.0 / 2 * (3 - x.y) * exp2(x.x, x.y)); }
+	[](vec2 x) { return -((1.0 - x.x) * exp1(x.x, x.y) + 2.0 / 3.0 * (2.0 - x.x) * exp2(x.x, x.y)); },
+	[](vec2 x) { return -(4.0 * (1.0 - x.y) * exp1(x.x, x.y) + 3.0 / 2.0 * (3.0 - x.y) * exp2(x.x, x.y)); }
 };
 
 hesse h2 =
 {
-	[](vec2 x) { return -((0.5 * (1 - x.x) * (1 - x.x) - 1) * exp1(x.x, x.y) + 2.0 / 3 * (2.0 / 9 * (2 - x.x) * (2 - x.x) - 1) * exp2(x.x, x.y)); },
-	[](vec2 x) { return -(2 * (1 - x.x) * (1 - x.y) * exp1(x.x, x.y) + 1.0 / 3 * (2 - x.x) * (3 - x.y) * exp2(x.x, x.y)); },
-	[](vec2 x) { return -(2 * (1 - x.x) * (1 - x.y) * exp1(x.x, x.y) + 1.0 / 3 * (2 - x.x) * (3 - x.y) * exp2(x.x, x.y)); },
-	[](vec2 x) { return -(4 * (2 * (1 - x.y) * (1 - x.y) - 1) * exp1(x.x, x.y) + 1.5 * (0.5 * (3 - x.y) * (3 - x.y) - 1) * exp2(x.x, x.y)); }
+	[](vec2 x) { return -((0.5 * (1.0 - x.x) * (1.0 - x.x) - 1.0) * exp1(x.x, x.y) + 2.0 / 3.0 * (2.0 / 9.0 * (2.0 - x.x) * (2.0 - x.x) - 1.0) * exp2(x.x, x.y)); },
+	[](vec2 x) { return -(2.0 * (1.0 - x.x) * (1.0 - x.y) * exp1(x.x, x.y) + 1.0 / 3.0 * (2.0 - x.x) * (3.0 - x.y) * exp2(x.x, x.y)); },
+	[](vec2 x) { return -(2.0 * (1.0 - x.x) * (1.0 - x.y) * exp1(x.x, x.y) + 1.0 / 3.0 * (2.0 - x.x) * (3.0 - x.y) * exp2(x.x, x.y)); },
+	[](vec2 x) { return -(4.0 * (2.0 * (1.0 - x.y) * (1.0 - x.y) - 1.0) * exp1(x.x, x.y) + 1.5 * (0.5 * (3.0 - x.y) * (3.0 - x.y) - 1.0) * exp2(x.x, x.y)); }
 };
 
 int main()
 {
 	newton_info info;
-	info.x0 = vec2(2, 2);
+	double x1 = 1;					// Начальное значение x
+	double y1 = 2;					// Начальное значение y
+	info.x0 = vec2(x1, y1);
 	info.f = f2;					// Функция
 	info.g = g2;					// Градиент
 	info.h = h2;					// Матрица вторых производных
 	info.maxiter = 100000;			// Максимальное количество итераций
 	info.minimize_eps = 1.0e-12;	// Эпсилон для одномерной минимизаций
-	info.delta = 0.001;				// Дельта для ||x(k+1) - x(k)||
-	info.eps = 0.001;				// Эпсилон для |f(k+1) - f(k)|
+	info.delta = 1.0e-12;			// Дельта для ||x(k+1) - x(k)||
 
 	std::vector<vec2> points1, points2, points3;	// Вектор точек
 	result_info res1, res2, res3;					// Количество итераций + количество вычислений функций
 
-	res1 = lambda_newton(info, points1);
-	vec2 min1 = points1.back();
+	std::ofstream out;
+	out.open("result.csv");
+	out << "Точность;Начальное значение x;Начальное значение y;Количество итераций;Количество вычислений;Найденное значение x;Найденное значение y;Значение функции" << std::endl;
+	for (double eps = 1.0e-3; eps > 1.0e-7; eps *= 1.0e-1)
+	{
+		info.eps = eps;				// Эпсилон для |f(k+1) - f(k)|
+		res1 = lambda_newton(info, points1);
+		vec2 min1 = points1.back();
 
-	//Вывод всего что есть
-	points1.clear();
+		// Вывод
+		out << eps << ";" << x1 << ";" << y1 << ";" << res1.iter_count << ";" << res1.calc_count << ";" << min1.x << ";" << min1.y << ";" << info.f(min1) << std::endl;
 
+		points1.clear();
+	}
+	out.close();
 
 	broyden_info b_info;
 	b_info.f = f2;
 	b_info.g = g2;
-	b_info.eps = 0.001;
-	b_info.minimize_eps = 1.0e-8;
+	b_info.minimize_eps = 1.0e-12;
 	b_info.maxiter = 100000;
-	b_info.x0 = vec2(2, 2);
+	b_info.x0 = vec2(x1, y1);
 
-	res3 = broyden(b_info, points3);
+	//std::ofstream out;
+	//out.open("result.csv");
+	//out << "Точность;Начальное значение x;Начальное значение y;Количество итераций;Количество вычислений;Найденное значение x;Найденное значение y;Значение функции" << std::endl;
+	//for (double eps = 1.0e-3; eps > 1.0e-7; eps *= 1.0e-1)
+	//{
+	//	b_info.eps = eps;				// Эпсилон для |f(k+1) - f(k)|
+	//	res3 = broyden(b_info, points3);
+	//	vec2 min3;
+	//	if (points3.size() >= 2)
+	//		min3 = points3[points3.size() - 2];
+	//	else
+	//		min3 = points3.back();
 
-	vec2 min = points3[points3.size() - 2];
+	//	// Вывод
+	//	out << eps << ";" << x1 << ";" << y1 << ";" << res3.iter_count << ";" << res3.calc_count << ";" << min3.x << ";" << min3.y << ";" << b_info.f(min3) << std::endl;
+
+	//	points3.clear();
+	//}
+	//out.close();
+
+	system("result.csv");
 	return 0;
 }
